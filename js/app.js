11bed469760a570312b4953de120f3970d6788eb
FusionFroge <fusionfroge@gmail.com>
2024-12-13 07:29:42 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// typer
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed("#typed-text", {
+      strings: ["...","Cruz Inn", "privacy and Safty", "Luxury and Comfort"], // List of text strings
+      typeSpeed: 230, // Typing speed in milliseconds
+      backSpeed: 60,  // Backspacing speed
+      loop: true,     // Repeat the typing effect
+    });
+  });
